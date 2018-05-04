be82f50967f160fb3b61cdb896311412bed50697
David <waywardblues@gmail.com>
2018-05-04 22:10:45 +0700

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = React.createClass ({
+  render: function () {
+    return (
+      <h3>Countdown</h3>
+    )
+  }
+});
+
+module.exports = Countdown;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,14 +1,12 @@
 var React = require('react');
 var Navigation = require('Navigation');
 
-
 var Main = (props) => {
   return (
     <div>
       <div>
         <Navigation/>
         <div>
-          <h1>Timer Main component.</h1>
           {props.children}
         </div>
       </div>
